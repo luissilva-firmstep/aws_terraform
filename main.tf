bbf31aa4d8b27c18ea9388491c70e5bc867192bc
Luis Silva <nandonespolo@gmail.com>
2018-08-10 15:00:13 +0000

    Updates

--- main.tf
+++ main.tf
@@ -61,14 +61,76 @@ resource "aws_subnet" "private1" {
   }
 }
 
-resource "aws_subnet" "private2" {
-  vpc_id = "${aws_vpc.vpc.id}"
-  cidr_block = "10.1.3.0/24"
-  map_public_ip_on_launch = false
-  availability_zone = "${data.aws_availability_zones.available.names[0]}"
+# Subnet Associations
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id = "${aws_subnet.public.id}"
+  route_table_id = "${aws_route_table.public.id}"
+}
 
-  tags {
-    Name = "private2"
+resource "aws_route_table_association" "private1_assoc" {
+  subnet_id = "${aws_subnet.private1.id}"
+  route_table_id = "${aws_route_table.public.id}"
+}
+
+# Security groups
+# Public Security Group
+resource "aws_security_group" "public" {
+  name = "sg_public"
+  description = "Used for public and private instances for load balancer access"
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  # SSH
+  ingress {
+    from_port   = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  # HTTP
+  ingress {
+    from_port   = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Outbound internet access
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+# Private Security Group
+resource "aws_security_group" "private" {
+  name        = "sg_private"
+  description = "Used for private instances"
+  vpc_id      = "${aws_vpc.vpc.id}"
+
+
+  # Access from other security groups
+
+  ingress {
+    from_port    = 0
+    to_port      = 0
+    protocol     = "-1"
+    cidr_blocks  = ["10.1.0.0/16"]
+  }
+
+  egress {
+    from_port    = 0
+    to_port      = 0
+    protocol     = "-1"
+    cidr_blocks  = ["0.0.0.0/0"]
+  }
+}
+
+
+
+
+
 
