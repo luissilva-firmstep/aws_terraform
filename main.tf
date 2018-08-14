8582b7da73e13344667a1e6c37b779ba08c00381
Luis Silva <nandonespolo@gmail.com>
2018-08-14 15:21:01 +0000

    Updates

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
   profile = "${var.aws_profile}"
 }
 
-data "aws_availability_zones" "available" {}
+#data "aws_availability_zones" "available" {}
 
 # VPC
 resource "aws_vpc" "vpc" {
@@ -44,21 +44,21 @@ resource "aws_subnet" "public" {
   vpc_id = "${aws_vpc.vpc.id}"
   cidr_block = "10.1.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone = "${data.aws_availability_zones.available.names[0]}"
+#  availability_zone = "${data.aws_availability_zones.available.names[0]}"
 
   tags {
     Name = "public"
   }
 }
 
-resource "aws_subnet" "private1" {
+resource "aws_subnet" "private" {
   vpc_id = "${aws_vpc.vpc.id}"
   cidr_block = "10.1.2.0/24"
   map_public_ip_on_launch = false
-  availability_zone = "${data.aws_availability_zones.available.names[1]}"
+#  availability_zone = "${data.aws_availability_zones.available.names[1]}"
 
   tags {
-    Name = "private1"
+    Name = "private"
   }
 }
 
@@ -68,8 +68,8 @@ resource "aws_route_table_association" "public_assoc" {
   route_table_id = "${aws_route_table.public.id}"
 }
 
-resource "aws_route_table_association" "private1_assoc" {
-  subnet_id = "${aws_subnet.private1.id}"
+resource "aws_route_table_association" "private_assoc" {
+  subnet_id = "${aws_subnet.private.id}"
   route_table_id = "${aws_route_table.public.id}"
 }
 
@@ -131,7 +131,26 @@ resource "aws_security_group" "private" {
 }
 
 
+# key pair
+resource "aws_key_pair" "auth" {
+  key_name  ="${var.key_name}"
+  public_key = "${file(var.public_key_path)}"
+}
+
+# Server
+resource "aws_instance" "dev" {
+  instance_type = "${var.dev_instance_type}"
+  ami = "${var.dev_ami}"
+  tags {
+    Name = "web-wordpress"
+  }
+
+  key_name = "${aws_key_pair.auth.id}"
+  vpc_security_group_ids = ["${aws_security_group.public.id}"]
+  subnet_id = "${aws_subnet.public.id}"
+}
 
 
 
 
+######## ...to be continue.d
--- variables.tf
+++ variables.tf
@@ -10,3 +10,5 @@ variable "key_name" {}
 variable "public_key_path" {}
 variable "dev_instance_type" {}
 variable "dev_ami" {}
+variable "db_instance_type" {}
+variable "db_ami" {}
